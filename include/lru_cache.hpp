e36edd707b01181892af95e02f471bbdb4103de6
Ivis <ivan.e.minsk@gmail.com>
2024-11-11 22:00:36 +0000

    Init

---
+++ include/lru_cache.hpp
@@ -0,0 +1,63 @@
+#ifndef LRU_CACHE_HPP 
+#define LRU_CACHE_HPP 
+ 
+#include <unordered_map> 
+#include <list> 
+#include <optional> 
+#include <memory> 
+#include <string> 
+ 
+template <typename Key, typename Value> 
+class LRUCache { 
+
+public: 
+    explicit LRUCache(size_t capacity) : capacity_(capacity) {} 
+
+    static std::unique_ptr<LRUCache<Key, Value>> Create(size_t capacity) { 
+        if (capacity == 0) { 
+            return nullptr; 
+        } 
+        return std::make_unique<LRUCache<Key, Value>>(capacity); 
+    } 
+ 
+   
+    void put(const Key& key, const Value& value) { 
+        auto it = cache_map_.find(key); 
+        if (it != cache_map_.end()) { 
+            cache_list_.erase(it->second); 
+        } 
+ 
+        cache_list_.emplace_front(key, value); 
+        cache_map_[key] = cache_list_.begin(); 
+ 
+        if (cache_list_.size() > capacity_) { 
+            auto last = cache_list_.end(); 
+            --last; 
+            cache_map_.erase(last->first); 
+            cache_list_.pop_back(); 
+        } 
+    } 
+ 
+    
+    std::optional<Value> get(const Key& key) { 
+        auto it = cache_map_.find(key); 
+        if (it == cache_map_.end()) { 
+            return std::nullopt; 
+        } 
+ 
+         
+        cache_list_.splice(cache_list_.begin(), cache_list_, it->second); 
+        return it->second->second; 
+    } 
+ 
+private: 
+    size_t capacity_; 
+    std::list<std::pair<Key, Value>> cache_list_; 
+    std::unordered_map<Key, typename std::list<std::pair<Key, Value>>::iterator> cache_map_; 
+}; 
+ 
+
+template class LRUCache<int, std::string>; 
+template class LRUCache<std::string, int>; 
+ 
+#endif 
---
+++ tests/LRUCacheTest.cpp
@@ -0,0 +1,55 @@
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch_all.hpp>
+#include "../include/lru_cache.hpp"
+#include <iostream>
+
+TEST_CASE("LRUCache<int, std::string>", "[LRUCache]") {
+    auto cache = LRUCache<int, std::string>::Create(2);
+    REQUIRE(cache != nullptr);
+
+    SECTION("Inserting and retrieving elements") {
+        cache->put(1, "one");
+        cache->put(2, "two");
+        INFO("1");
+        REQUIRE(cache->get(1).has_value());
+        REQUIRE(cache->get(1).value() == "one");
+        REQUIRE(cache->get(2).has_value());
+        REQUIRE(cache->get(2).value() == "two");
+    }
+
+    SECTION("Eviction of least recently used element") {
+        cache->put(1, "one");
+        cache->put(2, "two");
+        cache->put(3, "three"); 
+        INFO("2");
+       // std::cout <<"test 32423423 \n";
+        REQUIRE(!cache->get(1).has_value());
+        REQUIRE(cache->get(2).has_value());
+        REQUIRE(cache->get(3).has_value());
+    }
+}
+
+TEST_CASE("LRUCache<std::string, int>", "[LRUCache]") {
+    auto cache = LRUCache<std::string, int>::Create(2);
+    REQUIRE(cache != nullptr);
+
+    SECTION("Inserting and retrieving elements") {
+        cache->put("one", 1);
+        cache->put("two", 2);
+
+        REQUIRE(cache->get("one").has_value());
+        REQUIRE(cache->get("one").value() == 1);
+        REQUIRE(cache->get("two").has_value());
+        REQUIRE(cache->get("two").value() == 2);
+    }
+
+    SECTION("Eviction of least recently used element") {
+        cache->put("one", 1);
+        cache->put("two", 2);
+        cache->put("three", 3); 
+
+        REQUIRE(!cache->get("one").has_value());
+        REQUIRE(cache->get("two").has_value());
+        REQUIRE(cache->get("three").has_value());
+    }
+}
