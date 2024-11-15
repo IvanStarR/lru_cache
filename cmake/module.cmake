260f9c22577e3b6f04726715859688c8e0f4fe99
Ivis <ivan.e.minsk@gmail.com>
2024-11-15 09:46:42 +0000

    fix macro

--- cmake/module.cmake
+++ cmake/module.cmake
@@ -1,38 +1,14 @@
-# Creates static or shared library or an exacutable binary from files of current directory with .cpp, .h, h++, .c, .cc, .hxx, .cxx extensions
-# NAME -- name of application
-# TYPE -- type of application. possible values are static, shared, executable
-# DEPENDENCIES -- list of deps of the app
-
-macro(application NAME)
-
-    set(DEPS ${ARGN})
-
-   
-    file(GLOB_RECURSE FILES
-        ${CMAKE_CURRENT_SOURCE_DIR}/*.cpp ${CMAKE_CURRENT_SOURCE_DIR}/*.c ${CMAKE_CURRENT_SOURCE_DIR}/*.cc
-        ${CMAKE_CURRENT_SOURCE_DIR}/*.cxx ${CMAKE_CURRENT_SOURCE_DIR}/*.h ${CMAKE_CURRENT_SOURCE_DIR}/*.h++
-        ${CMAKE_CURRENT_SOURCE_DIR}/*.hxx
-    )
-
-   
-    add_executable(${NAME} ${FILES})
-
-    target_include_directories(${NAME} PUBLIC ${CMAKE_CURRENT_SOURCE_DIR}/../../include)
-
-    foreach(DEP ${DEPS})
-        target_link_libraries(${NAME} PUBLIC ${DEP})
-    endforeach()
-endmacro()
-
-
-# Creates static or shared libraryfrom files of current directory with .cpp, .h, h++, .c, .cc, .hxx, .cxx extensions
+# Creates static or shared library from files of current directory with .cpp, .h, .c, .cc, .hxx, .cxx extensions
 # NAME -- name of a module
 # TYPE -- type of a module. possible values are static, shared
 # DEPENDENCIES -- list of deps of the module
 
-macro(module NAME TYPE) 
+macro(module)
+    cmake_parse_arguments(MODULE "" "NAME;TYPE" "DEPENDENCIES" ${ARGN})
 
-    set(DEPS ${ARGN})
+    if(NOT MODULE_NAME OR NOT MODULE_TYPE)
+        message(FATAL_ERROR "module macro requires NAME and TYPE arguments")
+    endif()
 
     file(GLOB_RECURSE MODULE_SOURCES
         ${CMAKE_CURRENT_SOURCE_DIR}/*.cpp ${CMAKE_CURRENT_SOURCE_DIR}/*.c ${CMAKE_CURRENT_SOURCE_DIR}/*.cc
@@ -40,13 +16,42 @@ macro(module NAME TYPE)
         ${CMAKE_CURRENT_SOURCE_DIR}/*.hxx
     )
 
-    add_library(${NAME} ${TYPE} ${MODULE_SOURCES})
+    add_library(${MODULE_NAME} ${MODULE_TYPE} ${MODULE_SOURCES})
 
-    target_include_directories(${NAME} PUBLIC $${CMAKE_CURRENT_SOURCE_DIR}/../../include)
+    target_include_directories(${MODULE_NAME} PUBLIC ${CMAKE_CURRENT_SOURCE_DIR}/../../include)
 
-    foreach(DEP ${DEPS})
-    target_link_libraries(${NAME} PUBLIC ${DEP})
-    endforeach()
-
+    if(MODULE_DEPENDENCIES)
+        foreach(DEP ${MODULE_DEPENDENCIES})
+            target_link_libraries(${MODULE_NAME} PUBLIC ${DEP})
+        endforeach()
+    endif()
 endmacro()
 
+# Creates static or shared library or an executable binary from files of current directory with .cpp, .h, .c, .cc, .hxx, .cxx extensions
+# NAME -- name of application
+# TYPE -- type of application. possible values are static, shared, executable
+# DEPENDENCIES -- list of deps of the app
+
+macro(application)
+    cmake_parse_arguments(APP "" "NAME;TYPE" "DEPENDENCIES" ${ARGN})
+
+    if(NOT APP_NAME OR NOT APP_TYPE)
+        message(FATAL_ERROR "application macro requires NAME and TYPE arguments")
+    endif()
+
+    file(GLOB_RECURSE APP_SOURCES
+        ${CMAKE_CURRENT_SOURCE_DIR}/*.cpp ${CMAKE_CURRENT_SOURCE_DIR}/*.c ${CMAKE_CURRENT_SOURCE_DIR}/*.cc
+        ${CMAKE_CURRENT_SOURCE_DIR}/*.cxx ${CMAKE_CURRENT_SOURCE_DIR}/*.h ${CMAKE_CURRENT_SOURCE_DIR}/*.h++
+        ${CMAKE_CURRENT_SOURCE_DIR}/*.hxx
+    )
+
+    add_executable(${APP_NAME} ${APP_SOURCES})
+
+    target_include_directories(${APP_NAME} PUBLIC ${CMAKE_CURRENT_SOURCE_DIR}/../../include)
+
+    if(APP_DEPENDENCIES)
+        foreach(DEP ${APP_DEPENDENCIES})
+            target_link_libraries(${APP_NAME} PUBLIC ${DEP})
+        endforeach()
+    endif()
+endmacro()
